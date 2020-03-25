0249e75886b5937c4d1089c782ce16a7946bc720
teobler <teobler@163.com>
2020-03-25 14:40:05 +0800

    add new path for response type.

--- src/PathResolver.ts
+++ src/PathResolver.ts
@@ -208,7 +208,9 @@ export class PathResolver {
       results: this.extraDefinitions,
       // TODO: handle other content type here
       schema:
-        get(responses, "200.content.application/json.schema") || get(responses, "201.content.application/json.schema"),
+        get(responses, "200.content.application/json.schema") ||
+        get(responses, "200.content.*/*.schema") ||
+        get(responses, "201.content.application/json.schema"),
     }).resolve();
 
   // TODO: when parameters has enum
