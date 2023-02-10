2e662e878e9406506fea2e9851a51af5514a1f58
teobler <teobler@163.com>
2023-02-10 13:56:03 +0800

    remove useless index.ts file.

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./resolvers/PathResolver";
-export * from "./resolvers/DefinitionsResolver";
-export * from "./resolvers/SchemaResolver";
-export * from "./utils/formatters";
-export * from "./utils/specifications";
-export * from "./utils/generators";
