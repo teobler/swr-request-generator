b18a44f9d848d7d98307ae5356d9dd71b9734887
teobler <teobler@163.com>
2023-03-04 11:40:03 +0800

    update vitest config
    Signed-off-by: teobler <teobler@163.com>

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,18 +6,14 @@ export default defineConfig({
   test: {
     globals: true,
     coverage: {
-      branches: 85,
-      lines: 85,
-      functions: 85,
-      statements: 85,
+      branches: 90,
+      lines: 90,
+      functions: 90,
+      statements: 90,
       reporter: ["text", "html", "clover", "json", "json-summary"],
+      include: ["src/**/*.ts"],
+      exclude: ["src/constants.ts", "src/**/__tests__/**"],
     },
-    include: [
-      "src/**/__tests__/*.test.ts",
-      "!src/__types__/**",
-      "!src/__tests__/**",
-      "!src/index.ts",
-      "!src/request/**",
-    ],
+    include: ["src/**/__tests__/*.test.ts"],
   },
 });
